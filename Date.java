e667594d9413a97b58eeb1c928ce2debdf5ab19e
Samuel Piecz <samuel.piecz@gmail.com>
2019-03-05 10:30:17 -0600

    Added date class.

---
+++ Date.java
@@ -0,0 +1,20 @@
+// Create a Date class with three private integer instance variables named day, month, year.  It has a constructor with three parameters for initializing the instance variables, and it has a method named daysSinceJan1().  Note:  It could have other methods.
+public class Date 
+{
+	private integer day;
+	private integer month;
+	private integer year;
+
+
+	public Date(day, month, year)
+	{
+		this.day = day;
+		this.month = month;
+		this.year= year;
+	}
+
+	// Just as its identifier suggests, the daysSinceJan1() method computes and returns the number of days since January 1 of the same year, including January 1 and the day in the Date object itself.
+	public int daysSinceJan1()
+	{
+	}
+}
